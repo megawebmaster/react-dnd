fea81546176c7fcf24836fa1912a562e68d07b79
Amadeusz Starzykiewicz <megawebmaster@gmail.com>
2021-12-20 17:33:38 +0100

    fix: backport fix for disconnecting connectors (react-dnd#3224)
    This commit backports fix for 14.0.4 that disconnects source,
    target and preview connectors on component unmount.

--- packages/core/react-dnd/src/hooks/useDrag.ts
+++ packages/core/react-dnd/src/hooks/useDrag.ts
@@ -46,10 +46,12 @@ export function useDrag<
 	useIsomorphicLayoutEffect(() => {
 		connector.dragSourceOptions = specRef.current.options || null
 		connector.reconnect()
+		return () => connector.disconnectDragSource()
 	}, [connector])
 	useIsomorphicLayoutEffect(() => {
 		connector.dragPreviewOptions = specRef.current.previewOptions || null
 		connector.reconnect()
+		return () => connector.disconnectDragPreview()
 	}, [connector])
 	return [result, connectDragSource, connectDragPreview]
 }
